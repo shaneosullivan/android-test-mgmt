2b6d18a28dcedeace914dca2778da38916bf6086
Shane O'Sullivan <shaneosullivan1@gmail.com>
2025-09-25 09:33:08 +0100

    Fix the APP_URL_BASE

--- lib/consts.ts
+++ lib/consts.ts
@@ -8,11 +8,16 @@ export const REQUIRED_ENV_VARS = [
   "FIREBASE_PROJECT_ID",
   "GOOGLE_SERVICE_ACCOUNT_EMAIL",
   "GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY",
+  "GOOGLE_CLIENT_ID",
+  "GOOGLE_CLIENT_SECRET",
+  "GOOGLE_REDIRECT_URI",
+  "JWT_SECRET",
+  "TOKEN_ENCRYPTION_KEY",
 ] as const;
 
 export const MAX_PROMOTIONAL_CODES = 1000;
 
 export const APP_URL_BASE =
   process.env.NODE_ENV === "production"
-    ? "https://your-domain.com"
+    ? `https://${process.env.VERCEL_URL || "your-domain.com"}`
     : "http://localhost:3016";
